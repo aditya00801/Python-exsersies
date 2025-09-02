8c76f8b31838c68a1e1043fbe18fb8b9854325fa
Aditya Kushwaha <146941579+aditya00801@users.noreply.github.com>
2025-09-03 01:34:58 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,6 @@
 num_1 = int(input("Enter the first number:"))
 op = input("Enter the operator:")
 num_2 = int(input("Enter the second number:"))
-
 if op == "+":
     result = num_1 + num_2
     print("result :",num_1 ,"+",num_2,"=", result)
@@ -12,10 +11,12 @@ elif op == "*":
     result = num_1 * num_2
     print("result :", num_1, "*", num_2, "=", result)
 elif op == "/":
-    if num_2 == 0:
-        print("Error! : devide by 0")
-    else:
+    try:
         result = num_1 / num_2
         print("result :", num_1, "/", num_2, "=", result)
+    except ZeroDivisionError:
+        print("Error: You cannot divide by zero") 
 else :
     print("Error :Invalid operation!")
+
+
